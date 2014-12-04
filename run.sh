e45b05a0c48f0811a62edda531d45375914ca759
Eduardo Trujillo <ed@chromabits.com>
2014-12-04 13:00:00 -0500

    Add option for ca-file

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ fi
 if [ -n "$SSL_CERT" ]; then
     echo "SSL certificate provided"
     echo -e "${SSL_CERT}" > /servercert.pem
-    export SSL="ssl crt /servercert.pem"
+    echo -e "${SSL_CA}" > /serverca.pem
+    export SSL="ssl crt /servercert.pem ca-file /serverca.pem"
 else
     echo "No SSL certificate provided"
 fi
